63154ead6ca53e1c135bb57c9cb64d27c77ba465
Paola <pen.castro@yavirac.edu.ec>
2020-02-06 05:52:56 -0500

    ok

--- Servidor/src/app.js
+++ Servidor/src/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.Router()); 
 app.set('port', 3000);
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 //rutas
+
 app.use(require('./rutas/ruta.js'));
 
 
---
+++ Servidor/src/interface/modelo/serviEntrega.js
@@ -0,0 +1,1 @@
+const Empresa = require('../interface')
